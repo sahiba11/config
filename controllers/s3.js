758e25478e212972827171a39256172ef0972cc6
sahiba11 <sahibasanghi@gmail.com>
2016-05-02 11:39:31 +0530

    minor changes

--- controllers/s3.js
+++ controllers/s3.js
@@ -1,7 +1,7 @@
 var aws = require('aws-sdk');
 var Promise = require('promise');
 
-aws.config.update({region: 'eu-west-1'});
+aws.config.update({region: config.s3Region});
 
 var s3 = new aws.S3();
 var Utils = module.exports = {};
@@ -9,7 +9,7 @@ var Utils = module.exports = {};
 Utils.upload = function(data) {
   return new Promise(function(success, reject) {
     var params = {
-      Bucket: 'testing.configuration',
+      Bucket: config.s3bucket,
       Key: 'configuration.json',
       Body: new Buffer(JSON.stringify(data)),
       ACL: 'public-read'
@@ -29,7 +29,7 @@ Utils.upload = function(data) {
 Utils.download = function() {
   return new Promise(function(success, reject) {
     var params = {
-      Bucket: 'testing.configuration',
+      Bucket: config.s3bucket,
       Key: 'configuration.json'
     };
 
